82fb5e5295f32f67f22fff41a65f2472e7ee24dd
joe-main <qmmplusfact@gmail.com>
2022-03-24 06:56:54 +0900

    part2

--- api/app/Http/Resources/UserResource.php
+++ api/app/Http/Resources/UserResource.php
@@ -14,6 +14,13 @@ class UserResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        // ここから削除する
+        // return [parent::toArray($request)];
+        // ここまで削除
+        // ここから追記
+        return [
+            'id' => $this->id
+        ];
+        // ここまで追記
     }
 }
